8ca3cf892aaf32e00bbe8bb1553b1b7e474cd707
withered <sergeyzezyanov1@gmail.com>
2026-05-20 18:54:27 +0700

    Delete duplicate upload to the storage

--- backend/tasks/preprocessing.py
+++ backend/tasks/preprocessing.py
@@ -95,7 +95,5 @@ def convert_point_cloud_task(point_cloud_id: uuid.UUID):
                         file_data=data,
                         session=uow.session
                     )
-                # save in the storage
-                storage.upload_file_locally(data.key, file_path)
 
     run_async(run_task())
